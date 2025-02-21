387dacf9312945f343bab0a3bf9b1bf216bc7933
Mason Dow <masoncdow@gmail.com>
2025-02-21 16:10:10 -0700

    Updated README, Modulair python data retrieval script, folder structure

---
+++ scripts/py_retrieve_modulair.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+from quantaq import APIClient
+
+def authenticate():
+    client = APIClient(api_key=API_KEY)
+    return client
+
+def get_sensors(client):
+    sensors = client.get_devices()
+    return sensors
+
+def download_data(client, serial_number, start, end, interval='1h'):
+    data = client.get_data(
+        device_id=serial_number,
+        start=start,
+        end=end,
+        interval=interval
+    )
+    return data
+
+def save_data(data, filename='data/raw/modulair_pm25.csv'):
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
